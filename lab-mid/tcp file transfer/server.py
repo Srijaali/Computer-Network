1ba2761f17996b920fd67b9c776e0d5b7867241b
Syeda Rija Ali <142867637+Srijaali@users.noreply.github.com>
2026-03-31 03:03:36 +0500

    Update server.py

--- lab-mid/tcp file transfer/server.py
+++ lab-mid/tcp file transfer/server.py
@@ -1,17 +1,25 @@
 import socket
-import threading
 
-#ip = socket.gethostbyname(socket.gethostname())
-s = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-s.bind(('localhost',9999))
+print("server starting")
+s = socket.socket(socket.AF_INET , socket.SOCK_STREAM)
+s.bind(('localhost' , 9990))
 s.listen()
-print("server waitning for connction")
+print("server listening")
 
 while True:
     c,addr = s.accept()
-    print(f"[new client] {addr} connected!")
+    print(f"connected to: {addr}")
 
-    filename = c.recv(1024).decode()
-    print(filename)
+    filename =c.recv(1024).decode()
+    print(f"recved filename: {filename}")
+    file = open(filename , "w")
+    c.send("filename recved successfully!".encode())
 
+    data = c.recv(1024).decode()
+    print("file data recieved.")
+    file.write(data)
+    c.send("fils data recvd".encode())
+
+    file.close()
     c.close()
+    print("disc")
